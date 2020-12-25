8bc74b2d6e7a1c4e7d579605be1ef3dd5a047c79
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2020-12-25 22:41:40 +0530

    [UPDATE] API Endpoints

--- utils/apiEndpoints.js
+++ utils/apiEndpoints.js
@@ -1,19 +1,19 @@
 module.exports = {
-    API_ENDPOINT: '/api',
-    API_DOCS: '/api/docs',
-    AUTH_ENDPOINT: '/api/auth',
+    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_DOCS: `${process.env.API_ENDPOINT}/docs`,
+    AUTH_ENDPOINT: `${process.env.API_ENDPOINT}/auth`,
     AUTH_SIGNUP: '/signup',
     AUTH_LOGIN: '/login',
     AUTH_VERIFY_EMAIL: '/verify/:token',
-    AUTH_VERIFY_FULL: '/api/auth/verify/',
+    AUTH_VERIFY_FULL: `${process.env.API_ENDPOINT}/auth/verify/`,
     AUTH_VERIFY_EMAIL_RESEND: '/resend',
     AUTH_RECOVER: '/recover',
     AUTH_CHECK_RESET_LINK: '/reset/:token',
     AUTH_RESET: '/reset/:token',
-    AUTH_RESET_FULL: '/api/auth/reset/',
+    AUTH_RESET_FULL: `${process.env.API_ENDPOINT}/auth/reset/`,
 
     // user
-    USER_ENDPOINT: '/api/user',
+    USER_ENDPOINT: `${process.env.API_ENDPOINT}/user`,
     USER_UPDATE: '/update',
     USER_UPDATE_PASSWORD: '/updatePassword',
     USER_DELETE_ACCOUNT: '/deleteAccount',
@@ -23,11 +23,11 @@ module.exports = {
     CLIENT_EMAIL_VERIFICATION_PATH: '/verifyEmail',
 
     // subscriptions
-    SUBSCRIPTIONS_ENDPOINT: '/api/subscriptions',
+    SUBSCRIPTIONS_ENDPOINT: `${process.env.API_ENDPOINT}/subscriptions`,
     SUBSCRIPTIONS_ADD_GOOGLE: '/add/google',
     SUBSCRIPTIONS_ADD_GOOGLE_CALLBACK: '/add/google/callback',
     SUBSCRIPTIONS_UPDATE_SUBSCRIPTION: '/updateSubscription',
 
     // spreedsheets
-    SPREADSHEETS_ENDPOINT: '/api/spreadsheets',
+    SPREADSHEETS_ENDPOINT: `${process.env.API_ENDPOINT}/spreadsheets`,
 };
